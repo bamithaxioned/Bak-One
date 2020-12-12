c760dfe710ce01634b862105d1f41a7f15390234
bamithprdxn <amithkumarb@prdxn.com>
2020-12-12 15:55:48 +0530

    Image Iterator Js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,28 @@
 $(document).ready(function () {
 
     // ---------------back to top js
-    $(".top-btn").click(() => $(window).scrollTop(0));
+    $(".top-btn").click(() => $(window).scrollTop(0));  
+
+
+    // iterator
+    $(".filter-links").click(function () {
+        let value = $(this).attr("data-filter");
+        if (value == "all") {
+            $(".image-list-li").show();
+            $(".image-list").css({
+                "justify-content": "space-between",
+                "margin-right" : "0"
+            })
+        } else {
+            $(".image-list").css({
+                "justify-content": "flex-start",
+                "margin-right" : "15px"
+            })
+            $(".image-list-li").not("." + value).hide();
+            $(".image-list-li").filter("." + value).show();
+
+        }
+    })
 
 })
 
